4a5edbd3cddb5ea0a8320067402b6fd7a0047f38
rahimovanvarbek61-dot <rahimovanvarbek61@gmail.com>
2025-10-25 17:30:31 +0500

    Add files via upload

---
+++ main (13).cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int t;
+cout<<"sonni kiriting: ";
+cin>>t;
+int s=0;
+for(int i=0; i<=t; i++){
+    s=s+i;
+}
+cout<<s;
+    return 0;
+}
